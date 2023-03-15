810ed3e5c5562c9463ed0ae4a147c0ea732a5afd
Helio Fernandes dos Reis Lima <helio_lima@outlook.com.br>
2023-03-15 18:22:42 -0300

    versao estavel

--- script/main.js
+++ script/main.js
@@ -1,4 +1,5 @@
 const url = "https://api.github.com/users/helio2323"
+const url_repos = "https://api.github.com/users/helio2323/repos"
 
 function getUser(){
     fetch(`${url}`)
@@ -6,8 +7,17 @@ function getUser(){
     .then(data => { 
         userName.textContent = data.name
         img.src = data.avatar_url
-    })
+        userLocation.textContent = data.location
+        job.textContent = data.company   
+        biog.textContent = data.bio  
+    })  
     .catch(error => console.error(error))
+}
+
+function getRepositories(){
+
+
+    
 }
 
 getUser()
